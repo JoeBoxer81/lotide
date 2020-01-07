2804a5f9e975e04d59107cbfaa70c0830f70ef29
Joseph Ng <josephng81work@gmail.com>
2020-01-07 04:09:06 +0000

    head

---
+++ head.js
@@ -0,0 +1,30 @@
+/*const sum = function (a, b) {
+  return a + b;
+}
+
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3);*/
+
+/*const sumBuggy = function(a, b) {
+  return a * b;
+}
+
+console.assert(sumBuggy(1, 2) === 3);*/
+
+const assertEqual = function(actual, expected) {
+
+
+  const head = function(arr) {
+   let firstArr = arr[0];
+   return firstArr;
+  }
+
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//if (actual === expected) {
+// console.log(assertEqual());
+
